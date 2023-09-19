79eff49d9150926cefb1ed5e54e3752b042303f9
zamuragin03 <zamuragin03@gmail.com>
2023-09-19 17:01:27 +0300

    new db volume

--- server/server/settings.py
+++ server/server/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': Path(BASE_DIR).joinpath('db').joinpath('db.sqlite3'),
     }
 }
 
